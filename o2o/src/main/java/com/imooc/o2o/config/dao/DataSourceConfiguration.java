54b038fd8cefd52e38eaa120a0bb9eda839f1df7
Zac5566 <68909459+Zac5566@users.noreply.github.com>
2020-08-03 13:19:12 +0800

    簡化配置dataSource
    利用@configurationProperties來簡化原先的配置

--- o2o/src/main/java/com/imooc/o2o/config/dao/DataSourceConfiguration.java
+++ o2o/src/main/java/com/imooc/o2o/config/dao/DataSourceConfiguration.java
@@ -1,12 +1,13 @@
 package com.imooc.o2o.config.dao;
 
-import com.imooc.o2o.util.DESUtil;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
 
 @Configuration
@@ -14,31 +15,10 @@ import java.beans.PropertyVetoException;
 @MapperScan("com.imooc.o2o.dao")
 public class DataSourceConfiguration {
 
-    @Value("${jdbc.driver}")
-    private String jdbcDriver;
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-    @Value("${jdbc.username}")
-    private String jdbcUsername;
-    @Value("${jdbc.password}")
-    private String jdbcPassword;
-
     @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "c3p0")
     public ComboPooledDataSource createDataSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(DESUtil.getDecryptString(jdbcUsername));
-        dataSource.setPassword(DESUtil.getDecryptString(jdbcPassword));
-        //c3p0私有屬性
-        dataSource.setMaxPoolSize(30);
-        dataSource.setMinPoolSize(10);
-        //關閉連結後不自動commit
-        dataSource.setAutoCommitOnClose(false);
-        //連接超時
-        dataSource.setCheckoutTimeout(10000);
-        //失敗重試次數
-        dataSource.setAcquireRetryAttempts(2);
-        return dataSource;
+        return DataSourceBuilder.create().type(com.mchange.v2.c3p0.ComboPooledDataSource.class).build();
     }
+
 }
